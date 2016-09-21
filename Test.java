02f830ab9c7b3ff845505e8e98ea7a31d1341c4c
kasponrepo <repository@kaspontech.com>
2016-09-21 15:31:17 +0530

    Test.java file changed

--- Test.java
+++ Test.java
@@ -1,17 +1,19 @@
 public class Test
 {
-  public Test()
+  private int n = 2;
+  public Test(int n)
   {
+	this.n = n;
   }
   
   public void displayNumber()
   {
-    System.out.println("1");
+    System.out.println(n);
   }
   
   public void main(String[] args)
   {
-    Test test = new Test();
+    Test test = new Test(5);
     test.displayNumber();
   }
 }
