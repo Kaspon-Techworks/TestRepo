38cf70c3856c23ac8999df2f73ef4f54070fe85f
kasponrepo <repository@kaspontech.com>
2016-09-21 15:20:02 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,18 @@
+public class Test
+{
+  public Test()
+  {
+  }
+  
+  public void displayNumber()
+  {
+    System.out.println("1");
+  }
+  
+  public void main(String[] args)
+  {
+    Test test = new Test();
+    test.displayNumber();
+  }
+}
+    
